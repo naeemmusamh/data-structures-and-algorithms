efd242beada45afbf1690b49e432cbe357d1ae6d
naeemmusamh <naeem@DESKTOP-RJ6QHBJ.localdomain>
2021-05-18 09:29:54 +0300

    new code challenge

---
+++ code-challenges401/linkedlist-insertions/Readme.md
@@ -0,0 +1,28 @@
+# Singly Linked List
+
+let's say we have multiple values and this value inside something call (node) and this node have a relation between it and it Connected with each other.
+
+## Challenge
+
+.append(value) which adds a new node with the given value to the end of the list
+
+.insertBefore(value, newVal) which add a new node with the given newValue immediately before the first value node
+
+.insertAfter(value, newVal) which add a new node with the given newValue immediately after the first value node
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+
+
+![linked list node](https://github.com/naeemmusamh/data-structures-and-algorithms/blob/master/code-challenges401/linked-list/linked%20list%20node.png?raw=true)
+
+ - [x] Top-level README “Table of Contents” is updated
+ - [x] Feature tasks for this challenge are completed
+ - [x] Unit tests written and passing
+     - [x] “Happy Path” - Expected outcome
+     - [x] Expected failure
+     - [x] Edge Case (if applicable/obvious)
+ - [x] README for this challenge is complete
+     - [x] Summary, Description, Approach & Efficiency, Solution
+     - [x] Link to code
+     - [x] Picture of whiteboard
---
+++ code-challenges401/linkedlist-insertions/__tests__/linked-list.test.js
@@ -0,0 +1,39 @@
+'use strict';
+
+//middleware for the linked list page
+const node = require('../linked-list.js')
+
+//route to check if the linked-test the functionally work correctly
+describe('test the node if the functionally work correctly', () => {
+    //route to check the head it exist
+    it('test if the head it not exist', () => {
+        let list = new node();
+        expect(list.head).toBeNull();
+    });
+
+    //route to check if the head have some value or data
+    it('test if the head have some value', () => {
+        let list = new node();
+        let headNode = 10;
+        list.append(headNode);
+        expect(list.head.data).toEqual(headNode);
+    });
+
+    //route to check if the node allow to base multiple value and node
+    it('test if the insert allow as to insert multiple value and node', () => {
+        let list = new node();
+        let headNode = 10;
+        list.append(headNode);
+        expect(list.head.data).toEqual(headNode);
+        let secundNode = 20;
+        list.append(secundNode);
+        expect(list.head.data).toEqual(secundNode);
+        expect(list.head.next.data).toEqual(headNode);
+        let tailNode = 30;
+        list.append(tailNode);
+        expect(list.head.data).toEqual(tailNode);
+        expect(list.head.next.data).toEqual(secundNode);
+        expect(list.head.next.next.data).toEqual(headNode);
+    });
+
+});
